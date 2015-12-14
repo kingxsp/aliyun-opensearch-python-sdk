4b6243b9e9eb9c671b133d50eace54166e00982f
金红波 <hbjin@bcnetech.com>
2015-12-14 19:23:12 +0800

    no message

--- test/config.py
+++ test/config.py
@@ -8,6 +8,7 @@ base_url = 'http://opensearch-cn-hangzhou.aliyuncs.com'
 index_name = 't_' + str(uuid.uuid4()).replace('-', '')[2:30]
 build_index_name = 'build_test_index_py27'
 
+
 try:
     import requests
     client_name = 'requests'
